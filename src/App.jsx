a24bc13cd3d7728ae0e4d54a1d3882ab11cf1137
OmVarma9 <omvarma369@gmail.com>
2025-11-21 07:59:56 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import Dashboard from './pages/dashboard'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 
 const App = () => {
   return (
     <>
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
       <Dashboard/>
     </>
   )
